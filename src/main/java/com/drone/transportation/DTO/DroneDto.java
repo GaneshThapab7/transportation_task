7a8cce3b867f79c6b05a67253db03e8e05ccc122
kkpb8 <ganesh.thapa@microwarecorp.com>
2024-01-02 22:30:50 +0545

    update serial Number error message

--- src/main/java/com/drone/transportation/DTO/DroneDto.java
+++ src/main/java/com/drone/transportation/DTO/DroneDto.java
@@ -10,7 +10,7 @@ public class DroneDto {
 
     @NotBlank(message = "Invalid Serial Number: Empty Serial Number")
     @NotNull(message = "Invalid Serial Number: Serial Number is NULL")
-    @Size(min = 0,max = 100,message = "Invalid Serial Number Length should be 0-100")
+    @Size(min = 10,max = 100,message = "Invalid Serial Number Length should be 10-100")
     private String serialNumber;
     @NotBlank(message = "Invalid Model: Empty Model")
     @NotNull(message = "Invalid Model: Model is NULL")
